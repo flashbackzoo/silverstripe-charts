be4fec761920c00a004055b1a5c8384e68d2446f
flashbackzoo <david@davidcraig.co.nz>
2017-05-06 17:50:28 +1200

    Fix unit tests

--- test/ChartTest.php
+++ test/ChartTest.php
@@ -1,24 +1,32 @@
 <?php
 
-class ChartTest extends SapphireTest {
+class ChartTest extends SapphireTest
+{
+    protected static $fixture_file = 'ChartTest.yml';
 
-    protected static $fixture_file = 'ChartsTest.yml';
-
-    public function setUp() {
+    public function setUp()
+    {
         parent::setUp();
 
-        if(!file_exists(ASSETS_PATH)) mkdir(ASSETS_PATH);
+        if (!file_exists(ASSETS_PATH)) {
+            mkdir(ASSETS_PATH);
+        }
 
         // Create a test folder for each of the fixture references
         $folderIDs = $this->allFixtureIDs('Folder');
-        foreach($folderIDs as $folderID) {
+
+        foreach ($folderIDs as $folderID) {
             $folder = DataObject::get_by_id('Folder', $folderID);
-            if(!file_exists(BASE_PATH."/$folder->Filename")) mkdir(BASE_PATH."/$folder->Filename");
+
+            if (!file_exists(BASE_PATH."/$folder->Filename")) {
+                mkdir(BASE_PATH."/$folder->Filename");
+            }
         }
 
         // Create a test file for each of the fixture references
         $fileIDs = $this->allFixtureIDs('File');
-        foreach($fileIDs as $fileID) {
+
+        foreach ($fileIDs as $fileID) {
             $file = DataObject::get_by_id('File', $fileID);
             $fh = fopen(BASE_PATH."/$file->Filename", "w");
             fwrite($fh, '"Option","Count"' . PHP_EOL);
@@ -30,32 +38,40 @@ class ChartTest extends SapphireTest {
         }
     }
 
-    public function tearDown() {
+    public function tearDown()
+    {
         parent::tearDown();
 
         // Remove the test files that we've created
         $fileIDs = $this->allFixtureIDs('File');
-        foreach($fileIDs as $fileID) {
+
+        foreach ($fileIDs as $fileID) {
             $file = DataObject::get_by_id('File', $fileID);
-            if($file && file_exists(BASE_PATH."/$file->Filename")) unlink(BASE_PATH."/$file->Filename");
+
+            if ($file && file_exists(BASE_PATH."/$file->Filename")) {
+                unlink(BASE_PATH."/$file->Filename");
+            }
         }
 
         // Remove the test folders that we've crated
         $folderIDs = $this->allFixtureIDs('Folder');
-        foreach($folderIDs as $folderID) {
+
+        foreach ($folderIDs as $folderID) {
             $folder = DataObject::get_by_id('Folder', $folderID);
-            if($folder && file_exists(BASE_PATH."/$folder->Filename")) {
+
+            if ($folder && file_exists(BASE_PATH."/$folder->Filename")) {
                 Filesystem::removeFolder(BASE_PATH."/$folder->Filename");
             }
         }
 
         // Remove left over folders and any files that may exist
-        if(file_exists(ASSETS_PATH.'/ChartTest')) {
+        if (file_exists(ASSETS_PATH.'/ChartTest')) {
             Filesystem::removeFolder(ASSETS_PATH.'/ChartTest');
         }
     }
 
-    public function testGetChartData() {
+    public function testGetChartData()
+    {
         $pieChart = $this->objFromFixture('Chart', 'chart1');
         $barChart = $this->objFromFixture('Chart', 'chart2');
 
